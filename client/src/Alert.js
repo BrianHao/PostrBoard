23433b221193583b0603e2cb0aac1a1a8c623c11
Brian Hao <brianhao@gmail.com>
2019-05-07 16:25:34 -0400

    testing alert

--- client/src/Alert.js
+++ client/src/Alert.js
@@ -11,21 +11,21 @@ export default function Alert(props) {
           case 'loginSuccess':
             return <UncontrolledAlert color="success">Successfully logged in as {sessionStorage.getItem('username')}!</UncontrolledAlert>;
           case 'loginError':
-            return <UncontrolledAlert color="error">Error: Incorrect credentials. Please try again.</UncontrolledAlert>;
+            return <UncontrolledAlert color="danger">Error: Incorrect credentials. Please try again.</UncontrolledAlert>;
           case 'logoutSuccess':
             return <UncontrolledAlert color="success">Successfully logged out.</UncontrolledAlert>;
           case 'signupSuccess':
             return <UncontrolledAlert color="success">Successfully signed up! You may now log in.</UncontrolledAlert>;
           case 'signupError':
-            return <UncontrolledAlert color="error">Error: Unable to register account with this username.</UncontrolledAlert>;
+            return <UncontrolledAlert color="danger">Error: Unable to register account with this username.</UncontrolledAlert>;
           case 'createBoardSuccess':
             return <UncontrolledAlert color="success">Successfully created Board!</UncontrolledAlert>;
           case 'createBoardError':
-            return <UncontrolledAlert color="error">Error: Could not create Board. Please ensure board name is unique and title is not empty.</UncontrolledAlert>;
+            return <UncontrolledAlert color="danger">Error: Could not create Board. Please ensure board name is unique and title is not empty.</UncontrolledAlert>;
           case 'createPostSuccess':
             return <UncontrolledAlert color="success">Successfully created Post!</UncontrolledAlert>;
           case 'createPostError':
-            return <UncontrolledAlert color="error">Error: Could not create Post. Please ensure title is not empty.</UncontrolledAlert>;
+            return <UncontrolledAlert color="danger">Error: Could not create Post. Please ensure title is not empty.</UncontrolledAlert>;
           default:
             return null;
         }
